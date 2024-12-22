1df27d20aa42fd3632ba19b166a9fb80cde20f63
theatiq <theatiq@gmail.com>
2024-12-22 21:33:47 +0600

    Server side Project of Atiqurs Tech Started

---
+++ README.md
@@ -0,0 +1,54 @@
+1. My Project:
+    Name          : "Crazy Gamer : A Game Review Application"
+    Hosted at     : Vercel (Server-Side) and Firebase (Client-Side).
+    Category      : Camellia
+    Github Client : "https://github.com/programming-hero-web-course2/b10-a10-client-side-theatiq"
+    Github Server : "https://github.com/programming-hero-web-course2/b10-a10-server-side-theatiq"
+    Live Client   : "https://game-master-atiqur.web.app/"
+    Live Server   : "https://assignment-10-atiqur-server.vercel.app/"
+
+2. A brief description:
+    Crazy Gamer is a dynamic platform designed for gaming enthusiasts to explore, share, and engage with game reviews. It features a user-friendly interface where visitors can read detailed reviews, view ratings, and explore genres for various games. Registered users can contribute their insights by adding reviews, sharing ratings, and curating personal watchlists. The platform supports seamless navigation, personalized experiences, and quick access to trending games. With a vibrant community and valuable content, Crazy Gamer empowers users to make informed gaming decisions and connect with like-minded gamers.
+
+3. List of React Fundamental concepts used in the project:
+    a. React Router
+    b. Virtual DOM
+    c. Browser Router
+    d. Different Hook
+    e. State Manage
+    f. Firebase
+    g. Context API
+    h. MongoDB
+
+4. Technologies used:
+    This is a modern Single Page Application. We have used cutting-edge technologies here. Some of those are as follows:
+    a. JavaScript
+    b. React with Vite
+    c. Tailwind CSS
+    d. DaisyUI
+    e. React Router
+    f. Firebase Authentication
+    g. Vercel and Firebase Hosting
+
+5. 5 key features of your project:
+    a. Comprehensive Game Reviews: Users can access detailed reviews and ratings for a variety of games across different genres and publishing years.
+    b. User-Generated Content: Registered users can contribute by adding their own reviews, sharing opinions, and rating games.
+    c. Game Watchlist: Users can create and manage personalized watchlists for games they want to keep track of.
+    d. Intuitive Navigation: A responsive and visually appealing design ensures easy browsing on both desktop and mobile devices.
+    e. Community Engagement: Features like user profiles and tooltips promote interaction and engagement within the gaming community.
+
+6. Have used the following packages:
+    "animate.css": "^4.1.1",
+    "animate.css": "^4.1.1",
+    "firebase": "^11.0.2",
+    "localforage": "^1.10.0",
+    "match-sorter": "^8.0.0",
+    "react": "^18.3.1",
+    "react-dark-mode-toggle": "^0.2.0",
+    "react-dom": "^18.3.1",
+    "react-icons": "^5.4.0",
+    "react-router-dom": "^6.28.0",
+    "react-simple-typewriter": "^5.0.1",
+    "react-tooltip": "^5.28.0",
+    "sort-by": "^1.2.0",
+    "sweetalert2": "^11.14.5"
---
+++ index.js
@@ -0,0 +1,145 @@
+require("dotenv").config()
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hnhnv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        const blogsCollection = client.db("blogsDB").collection('blogs')
+        const wishListCollection = client.db("blogsDB").collection("watchList")
+
+        app.get("/reviews", async (req, res) => {
+            const cursor = blogsCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+        // For home Page
+        app.get("/reviewsHome", async (req, res) => {
+            const cursor = blogsCollection.find().sort({ rating: -1 }).limit(6)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+
+        app.get("/review/:id", async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await blogsCollection.findOne(query)
+            res.send(result)
+        })
+        app.get("/watchList", async (req, res) => {
+            const cursor = wishListCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+
+        app.get("/watchList/:id", async (req, res) => {
+            const id = req.params.id
+            const query = { _id: id }
+            const result = await wishListCollection.findOne(query)
+            res.send(result)
+        })
+
+
+
+        // My Reviews
+        app.get("/myReviews", async (req, res) => {
+            const email = req.query.email
+            const filter = { email }
+            const result = await blogsCollection.find(filter).toArray()
+            res.send(result)
+        })
+        // My Watch List
+        app.get("/myWatchList", async (req, res) => {
+            const email = req.query.email
+            const filter = { email }
+            const result = await wishListCollection.find(filter).toArray()
+            res.send(result)
+        })
+
+
+        app.post("/addReview", async (req, res) => {
+            const newReview = req.body
+            console.log(newReview)
+            const result = await blogsCollection.insertOne(newReview)
+            res.send(result)
+        })
+
+        app.post("/watchList", async (req, res) => {
+            // const id = req.params.id
+            const watchList = req.body
+            console.log(watchList)
+            const result = await wishListCollection.insertOne(watchList)
+            res.send(result)
+        })
+
+        app.put("/review/:id", async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true };
+            const newReview = req.body
+            const updatedReview = {
+                $set: newReview
+
+            }
+            const result = await blogsCollection.updateOne(filter, updatedReview, options)
+            res.send(result)
+
+        })
+        // Delete from Review
+        app.delete("/review/:id", async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await blogsCollection.deleteOne(query)
+            res.send(result)
+        })
+        // Delete from Watch List
+        app.delete("/watchList/:id", async (req, res) => {
+            const id = req.params.id
+            const query = { _id: id }
+            const result = await wishListCollection.deleteOne(query)
+            res.send(result)
+        })
+
+
+        // Send a ping to confirm a successful connection
+        // todo Comment it before deployment.
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get("/", (req, res) => {
+    res.send("Assignment-11 server is running smoothly.......")
+})
+
+app.listen(port, () => {
+    console.log(`Assignment-11 server is running on Port: ${port}`)
+})
+
